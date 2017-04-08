de750d2d7bf87b8238b09d405cfd79d5ef6efa81
Bernardo C. Araujo <3ernardo@gmail.com>
2017-04-08 19:32:56 -0300

    Neighbours can now be identified

--- Islander.py
+++ Islander.py
@@ -1,17 +1,18 @@
 from random import randint
 
 # Definition of the Height and Width of the matrix;
-w, h = 10, 8;
-
-k, t = 0,0
+w, h = 10, 8
 
 matrix = [[randint(0, 1) for x in range(w)] for y in range(h)]
 islands = []
 
-k, t = 0,2
+### Printing the matrix and the top/left coordinates
+print ('==================\n'
+       ' MAPA\n'
+       '==================\n')
 
-x = 65;
-y = 65;
+x, y = 65, 65
+
 print(end='  ')
 for n in range (w):
     print (chr(x), end=' ')
@@ -25,13 +26,36 @@ for n in range (h):
         print (matrix[n][m], end=' '),
     print ()
 
-iCounter = 0
-iReference = 0
+### Testing for ones and printing it's coordinates
+print ('\n'
+       '==================\n'
+       ' COORDENADAS\n'
+       '==================\n')
+
+iCounter, iReference = 0, 0
 
 for iHCoord in range (h):
     for iWCoord in range (w):
         if matrix[iHCoord][iWCoord] == 1:
-            print ('Coordenadas: h', chr(iHCoord+65), ' w', chr(iWCoord+65))
+            print ('Coordenadas: h %s w %s' %(chr(iHCoord+65), chr(iWCoord+65)))
             iCounter += 1
             islands.append([iCounter, iHCoord, iWCoord, iReference])
-print (islands)
+
+### Testing for neighbours and printing
+print ('\n'
+       '==================\n'
+       ' VIZINHOS\n'
+       '==================\n')
+
+for n in range (iCounter):
+    print('Ilha %s:' %(islands[n][0]))
+    for m in range (iCounter):
+        #print ('compara %s com %s' %(islands[n], islands[m]))
+        if islands[n][1] == islands[m][1] and islands[n][2]+1 == islands[m][2]:
+            print ('- tem vizinho a direita')
+        if islands[n][1] == islands[m][1] and islands[n][2]-1 == islands[m][2]:
+            print ('- tem vizinho a esquerda')
+        if islands[n][2] == islands[m][2] and islands[n][1]+1 == islands[m][1]:
+            print ('- tem vizinho a baixo')
+        if islands[n][2] == islands[m][2] and islands[n][1]-1 == islands[m][1]:
+            print ('- tem vizinho a cima')
